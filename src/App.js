db9848ffca1a339985c65b336d0d02b322100706
Burhan Mohyuddin <burhan365@gmail.com>
2018-08-31 19:20:56 +0500

    robofriend modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { Robot } from './Robot';
+import './App.css';
+import Scroll from './Scroll.js';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            Robot: Robot,
+            Robot: [],
             searchfield: ' ',
         }
+    }
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ Robot: users }));
     }
     onSearch = (event) => {
         this.setState({ searchfield: event.target.value })
@@ -19,13 +25,20 @@ class App extends Component {
         const filterRobo = this.state.Robot.filter(Robot => {
             return Robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            < div className='tc ' >
-                <h1>ROBOFRIENDS</h1>
-                <SearchBox searchchange={this.onSearch} />
-                <CardList Robot={filterRobo} />
-            </div>
-        );
+        if (this.state.Robot.length === 0) {
+            return <h1>LOADING.....!</h1>
+        }
+        else {
+            return (
+                < div className='tc ' >
+                    <h1 className='f2' >ROBOFRIENDS</h1>
+                    <SearchBox searchchange={this.onSearch} />
+                    <Scroll>
+                        <CardList Robot={filterRobo} />
+                    </Scroll> 
+                </div>
+            );
+        }
     }
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-
+import './App.css'
 ReactDOM.render(
     <App/>, document.getElementById('root'));
 registerServiceWorker();
